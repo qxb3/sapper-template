7160f4f08c6f13b70e4c544077a7fe4f6ba957a6
qxb3 <toolong972@gmail.com>
2022-03-19 12:56:06 +0800

    delete images and some changes in index.svelte

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,10 +2,14 @@
 </script>
 
 <svelte:head>
-	<title>Movie Info</title>
-	<link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.0/css/all.min.css" rel="stylesheet">
-	<link href="//db.onlinewebfonts.com/c/a575313c6dc4fd00c1a9506e1c3ea4fc?family=Euclid+Circular+A" rel="stylesheet">
+	<title>Title</title>
+	<link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.0/css/all.min.css" rel="stylesheet" />
+	<link href="//db.onlinewebfonts.com/c/a575313c6dc4fd00c1a9506e1c3ea4fc?family=Euclid+Circular+A" rel="stylesheet" />
 	<style>
+		*, *:before, *:after {
+			box-sizing: inherit;
+			}
+
 		html, body {
 			font-family: 'Euclid Circular A';
 			box-sizing: border-box;
@@ -14,9 +18,9 @@
 	</style>
 </svelte:head>
 
-<p>component data</p>
+<p>component</p>
 <span class="icon">
-	<i class="fa-solid fa-address-card"></i>
+	<i class="fa-solid fa-house"></i>
 </span>
 
 <style>
