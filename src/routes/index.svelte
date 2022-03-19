ee2c8bed514495884228fe488ffc8df2ef5dc362
qxb3 <toolong972@gmail.com>
2022-03-19 11:03:28 +0800

    change font to Euclid Circular A

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -4,17 +4,17 @@
 <svelte:head>
 	<title>Movie Info</title>
 	<link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.0/css/all.min.css" rel="stylesheet">
-	<link href="https://fonts.googleapis.com/css?family=Didact Gothic" rel="stylesheet">
+	<link href="//db.onlinewebfonts.com/c/a575313c6dc4fd00c1a9506e1c3ea4fc?family=Euclid+Circular+A" rel="stylesheet">
 	<style>
 		html, body {
-			font-family: 'Didact Gothic';
+			font-family: 'Euclid Circular A';
 			box-sizing: border-box;
 			padding: 0;
 		}
 	</style>
 </svelte:head>
 
-<h1>Test</h1>
+<p>component data</p>
 <span class="icon">
 	<i class="fa-solid fa-address-card"></i>
 </span>
