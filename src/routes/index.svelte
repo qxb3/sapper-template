276ada71b1e6cd4dd83aa25d7f292352fb798fa1
qxb3 <toolong972@gmail.com>
2022-03-23 02:14:12 +0800

    move fontawesome and font in template.html

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -3,8 +3,6 @@
 
 <svelte:head>
 	<title>Title</title>
-	<link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.0/css/all.min.css" rel="stylesheet" />
-	<link href="//db.onlinewebfonts.com/c/a575313c6dc4fd00c1a9506e1c3ea4fc?family=Euclid+Circular+A" rel="stylesheet" />
 	<style>
 		*, *:before, *:after {
 			box-sizing: inherit;
